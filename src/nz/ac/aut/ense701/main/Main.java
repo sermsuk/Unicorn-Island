88ad1fb431cdd780ac4c7a70250e7dc1efce3527
sermsuk <isermsuk@yahoo.com>
2017-05-14 04:25:31 +1200

    changed music directory, last commit had error

--- src/nz/ac/aut/ense701/main/Main.java
+++ src/nz/ac/aut/ense701/main/Main.java
@@ -1,6 +1,17 @@
 package nz.ac.aut.ense701.main;
 
+import java.io.IOException;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import nz.ac.aut.ense701.gameModel.Game;
 import nz.ac.aut.ense701.gameModel.Menu;
+import nz.ac.aut.ense701.gui.KiwiCountUI;
 
 /**
  * Kiwi Count Project
@@ -10,6 +21,8 @@ import nz.ac.aut.ense701.gameModel.Menu;
  */
 public class Main 
 {
+    public static Mixer mixer;
+    public static Clip clip;
     /**
      * Main method of Kiwi Count.
      * 
@@ -17,9 +30,40 @@ public class Main
      */
     public static void main(String[] args) 
     {
+        System.out.print("asdfasf");
         Menu menu = new Menu();
         menu.Menu();
+        
+        Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
+        mixer = AudioSystem.getMixer(mixInfos[0]);
+        
+        DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+        try{
+            clip = (Clip)mixer.getLine(dataInfo);
+        }catch(LineUnavailableException e){
+            e.printStackTrace();
+        }
+        
+        try{
+            URL soundURL = sun.applet.Main.class.getResource("/Music/menusound.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+            clip.open(audioStream);
+        }catch(LineUnavailableException e){
+            e.printStackTrace();
+        }catch(UnsupportedAudioFileException e){
+           e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+        clip.start();
+        
+        do{
+            try{
+                Thread.sleep(50);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }while(clip.isActive());
     }
-    
-
 }
